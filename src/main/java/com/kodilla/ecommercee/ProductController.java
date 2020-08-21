4fb776e56e56ab61fcb3374259d53696d374a051
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-08-21 19:53:17 +0200

    JDP200802-1 v.1

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,48 @@
 package com.kodilla.ecommercee;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@Slf4j
+@CrossOrigin("*")
+@RequiredArgsConstructor
+@RequestMapping("/v1/product")
 public class ProductController {
+    private final ProductMapper productMapper;
+    private final ProductServiceDb productServiceDb;
+
+    @GetMapping
+    public List<ProductDto> getProducts() {
+        log.info("Get products");
+        return productMapper.mapToProductDto(productServiceDb.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ProductDto getProduct(@PathVariable final Long id) throws ProductNotFoundException {
+        log.info("Get product by ID: " + id);
+        return productMapper.mapToProductDto(productServiceDb.getProductById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product doesn't exist in database!")));
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    public void createProduct(@RequestBody final ProductDto productDto) {
+        log.info("Create product by: " + productDto);
+        productServiceDb.saveProduct(productMapper.mapToProduct(productDto));
+    }
+
+    @PutMapping
+    public ProductDto updateProduct(@RequestBody final ProductDto productDto) {
+        log.info("Update product by: " + productDto);
+        return productMapper.mapToProductDto(productServiceDb.saveProduct(productMapper.mapToProduct(productDto)));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable final Long id) {
+        log.info("Delete product by ID: " + id);
+        productServiceDb.deleteProductById(id);
+    }
 }
