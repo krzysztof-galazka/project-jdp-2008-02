549d8ef32283e6930f6c3a9552b150a52ee19d3b
hakhis <b2mieszkowski@gmail.com>
2020-08-24 16:04:08 +0200

    JDP200802-2 v0.2

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,33 +1,35 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.domain.GroupDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/v1/group")
 public class GroupController {
 
-    @GetMapping(value = "getGroups")
+    @GetMapping("getGroups")
     public List<GroupDTO> getGroups(){
 
         return new ArrayList<>();
     }
 
-    @PostMapping("createGroup")
+    @PostMapping
     public void createGroup(GroupDTO groupDTO){
 
     }
 
     @GetMapping("getGroup")
-    public GroupDTO getGroup(Long groupId){
+    public GroupDTO getGroup(Long groupId) throws GroupNotFoundException{
 
         return new GroupDTO(1L, "test name");
     }
 
-    @PutMapping("updateGroup")
+    @PutMapping
     public GroupDTO updateGroup(GroupDTO groupDTO){
 
         return new GroupDTO(1L, "updated test name");
