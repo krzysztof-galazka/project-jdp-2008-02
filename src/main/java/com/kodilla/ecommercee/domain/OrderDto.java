8c48fff9efbbf51ea2eebf34d75fe655d23b2036
F1iper <59711082+F1iper@users.noreply.github.com>
2020-08-25 20:09:13 +0200

    Jdp200802 3 (#7)
    * JDP200802-3
    
    * JDP200802-3
    
    * JDP200802-3 fix
    
    * JDP200802-3 fix v.2
    
    * pull from master
    
    * added pojo's + return types of controller methods
    
    * fix controller

---
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDto {
+    private Long id;
+    private String name;
+
+}
